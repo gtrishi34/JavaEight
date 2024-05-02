08733e4206c6c73c1347d5a9acd4b424983669dc
rishikesh ranjan <rishi448832@gmail.com>
2024-05-02 16:51:43 +0530

    fourth commit

---
+++ src/map/HshMap.java
@@ -0,0 +1,34 @@
+package map;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class HshMap {
+
+	public static void main(String[] args) {
+
+Map<String, Integer> emp = new HashMap<>();
+
+emp.put("rishi", 4000);
+emp.put("sohan", 2500);
+emp.put("abhi", 3300);
+emp.put("john", 4000);
+
+int rishiSal = emp.get("rishi");
+System.out.println("rishi salary is : "+ rishiSal);
+
+for(Map.Entry<String, Integer> e : emp.entrySet()){
+	
+	System.out.println(e.getKey()+e.getValue());
+	
+	 int max = emp.values().stream()
+             .mapToInt(Integer::intValue)
+             .max()
+             .orElse(Integer.MIN_VALUE);
+	 
+	 System.out.println("maximum salary :" +max);
+	
+}
+}
+}
